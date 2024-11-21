74a847d3a49c7c4f29fd68299cae217b85ea5da5
matheus henry <goncalvessanches.mh@gmail.com>
2024-11-20 23:39:58 -0300

    login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,41 +1,43 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack, useRouter } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack, useRouter } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 // import 'react-native-reanimated';
 
-import AuthProvider from '@/app/AuthContext';
-import { useAuth } from '@/hooks/useAuth';
+import AuthProvider from "@/app/AuthContext";
+import { useAuth } from "@/hooks/useAuth";
 
-// SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    SanFransciscoRegular:require('../assets/fonts/SFUIDisplay-Regular.otf'),
-    SanFransciscoBlack:require('../assets/fonts/SFUIDisplay-Black.otf'),
-    SanFransciscoBold:require('../assets/fonts/SFUIDisplay-Bold.ttf'),
-    SanFransciscoHeavy:require('../assets/fonts/SFUIDisplay-Heavy.otf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SanFransciscoRegular: require("../assets/fonts/SFUIDisplay-Regular.otf"),
+    SanFransciscoBlack: require("../assets/fonts/SFUIDisplay-Black.otf"),
+    SanFransciscoBold: require("../assets/fonts/SFUIDisplay-Bold.ttf"),
+    SanFransciscoHeavy: require("../assets/fonts/SFUIDisplay-Heavy.otf"),
     // SanFransciscoLigth:require('../assets/fonts/SFUIDisplay-Ligth.ttf'),
-    SanFransciscoMedium:require('../assets/fonts/SFUIDisplay-Medium.otf'),
-    SanFransciscoSemibold:require('../assets/fonts/SFUIDisplay-Semibold.otf'),
-    SanFransciscoThin:require('../assets/fonts/SFUIDisplay-Thin.otf'),
-
+    SanFransciscoMedium: require("../assets/fonts/SFUIDisplay-Medium.otf"),
+    SanFransciscoSemibold: require("../assets/fonts/SFUIDisplay-Semibold.otf"),
+    SanFransciscoThin: require("../assets/fonts/SFUIDisplay-Thin.otf"),
   });
-  const { isAuthenticated, loading } = useAuth(); 
+
+  const { isAuthenticated, loading } = useAuth();
   const router = useRouter();
+
   useEffect(() => {
-   
-    if (loaded && !loading) {
+    if (loaded) {
+      SplashScreen.hideAsync();
       if (!isAuthenticated) {
-        router.replace('/login');
-      } else {
-        SplashScreen.hideAsync();
+        router.replace("/login");
       }
     }
   }, [loaded, loading, isAuthenticated, router]);
-
 
   if (!loaded || loading) {
     return null;
@@ -43,12 +45,18 @@ export default function RootLayout() {
 
   return (
     <AuthProvider>
-      <ThemeProvider value={ DefaultTheme}>
+      <ThemeProvider value={DefaultTheme}>
         <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false, gestureEnabled: false }} />
+          <Stack.Screen
+            name="(tabs)"
+            options={{ headerShown: false, gestureEnabled: false }}
+          />
           <Stack.Screen name="person" options={{ headerShown: false }} />
-          <Stack.Screen name="profile" options={{ headerShown: false}} /> 
-          <Stack.Screen name="login" options={{ headerShown: false, gestureEnabled: false }} /> 
+          <Stack.Screen name="profile" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="login"
+            options={{ headerShown: false, gestureEnabled: false }}
+          />
           <Stack.Screen name="register" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
